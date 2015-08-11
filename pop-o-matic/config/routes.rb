a0907279a83bf4eb0cd0d5c828d77529212c808e
Erin Blauw <blau1302@student.nhl.nl>
2015-08-11 23:12:20 +0200

    add Competencies controller with index, new and _form

--- pop-o-matic/config/routes.rb
+++ pop-o-matic/config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'competencies/index'
+
+  resources :competencies
+
+  # get 'competencies/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'competencies#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
